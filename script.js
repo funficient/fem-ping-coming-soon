703a16d0cb32d5d56ba0b504a2e92f2f76b54183
karindames <karin.dames@hotmail.com>
2023-12-28 14:10:41 +0200

    Try to fix the validation

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
-const email = document.getElementById('email');
-const form = document.getElementById('form');
-const errorElement = document.getElementById('error');
+// const email = document.getElementById('email');
+// const form = document.getElementById('form');
+// const errorMessage = document.querySelector('error-text');
+// const buttonSubmit = document.querySelector(".button")
 
 
-function validateEmail(){
-    
-})
+// buttonSubmit.addEventListener('click', () => {
+//     if (email.value === '' || email.value == null || !email.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+//         errorMessage.classList.remove('hidden');
+//         email.style.border = "1px solid hsl(354, 100%, 66%)";
+//     } else {
+//         errorMessage.classList.add('hidden');
+//         email.style.border = "1px solid #222";
+//     }
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    if (email.value === '' || email.value == null) {
-        messages.push('Please enter a valid email address')
-    }
-    if (messages.length > 0) {
+// })
+
+
+let form = document.querySelector('form') 
+
+form.addEventListener('submit', e => {
     e.preventDefault()
-    errorElement.innerText = messages.join(', ')
-    }
+
+    let email = form.querySelector('input').value
+    let small = form.querySelector('small')
+
+    if (email.trim() === '') {
+        form.classList.add('error')
+        small.style.color = 'red'
+        email.style.border = "1px solid #ff5466"
+        small.innerHTML = 'Email cannot be empty'
+        
+     } else if (!valid(email)) {
+        form.classList.add('error')
+        small.style.color = 'red'
+        email.style.border = "1px solid #ff5466"
+        small.innerHTML = 'Please provide a valid email'
+
+     } else {
+        small.style.color = 'green'
+        small.innerHTML = 'Email confirmed'
+
+        setTimeout(() => {
+        form.classList.remove('error')
+        }, 1000);
+     }
 })
+
+function valid(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(String(email).toLocaleLowerCase());
+}
