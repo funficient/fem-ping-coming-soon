eb38139a31de465b6e6c271503dd07e364f6bac2
karindames <karin.dames@hotmail.com>
2023-12-30 16:49:47 +0200

    Edit validation

--- script.js
+++ script.js
@@ -1,53 +1,39 @@
-// const email = document.getElementById('email');
-// const form = document.getElementById('form');
-// const errorMessage = document.querySelector('error-text');
-// const buttonSubmit = document.querySelector(".button")
+const button = document.querySelector(`.button`);
+const email = document.querySelector('input');
+const response = document.querySelector(".error-text");
+const input = document.querySelector('[name="email"]');
+const inputBox = document.querySelector(".input-box");
 
 
-// buttonSubmit.addEventListener('click', () => {
-//     if (email.value === '' || email.value == null || !email.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-//         errorMessage.classList.remove('hidden');
-//         email.style.border = "1px solid hsl(354, 100%, 66%)";
-//     } else {
-//         errorMessage.classList.add('hidden');
-//         email.style.border = "1px solid #222";
-//     }
+function click (event) {
 
-// })
+    let emailid = email.value;
+    event.preventDefault();
+
+     if(emailid.length===0) {
+     response.textContent = "Email address cannot be blank!";
+     inputBox.classList.add("error");
+     inputBox.classList.remove("success");
+     response.classList.remove("success");
+
+     }else if(!emailid.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+      response.textContent = "Please provide a valid email address";
+      inputBox.classList.add("error");
+      inputBox.classList.remove("success");
+      response.classList.remove("success");
+      
+
+    }else {
+      response.textContent = "You have sucessfully subscribed, Thank you!";
+      inputBox.classList.remove("error");
+      inputBox.classList.add("success");
+      response.classList.add("success");
+      input.value = '';
+      
+    }
+    
+  }
+
+button.addEventListener(`click`, click);
 
 
-let form = document.querySelector('form') 
-
-form.addEventListener('submit', e => {
-    e.preventDefault()
-
-    let email = form.querySelector('input').value
-    let small = form.querySelector('small')
-
-    if (email.trim() === '') {
-        form.classList.add('error')
-        small.style.color = 'red'
-        email.style.border = "1px solid #ff5466"
-        small.innerHTML = 'Email cannot be empty'
-        
-     } else if (!valid(email)) {
-        form.classList.add('error')
-        small.style.color = 'red'
-        email.style.border = "1px solid #ff5466"
-        small.innerHTML = 'Please provide a valid email'
-
-     } else {
-        small.style.color = 'green'
-        small.innerHTML = 'Email confirmed'
-
-        setTimeout(() => {
-        form.classList.remove('error')
-        }, 1000);
-     }
-})
-
-function valid(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    return re.test(String(email).toLocaleLowerCase());
-}
